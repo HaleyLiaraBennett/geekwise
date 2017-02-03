75ad368e303958248763f965886d921db5a8b759
Gus <geekwise.gustavo.jimenez@gmail.com>
2017-02-03 15:49:32 -0700

    yugioohhhhhh

---
+++ feb03-js-strings/js/script.js
@@ -0,0 +1,19 @@
+var firstString = 'A literal string';
+var secondString = new String('A string object');
+
+var concatString = firstString + secondString;
+
+
+// var getIndex = firstString.match('it');
+// console.log(getIndex);
+
+var firstName = 'Haley';
+var lastName = 'Bennett';
+var birthDay = '1999';
+var year = '2020';
+var newAge = year - birthDay;
+ console.log(newAge);
+
+
+var info = "My name is " + firstName + " Liara " + lastName + ". I was born on " + birthDay + " and in " + year + ", I will be " + newAge + '.';
+console.log(info);
